5ef8e7964196c23876270e535fb3073913d7bb89
Dmitriy <madaminov.d@gmail.com>
2025-06-26 18:01:15 +0500

    fix

--- assets/template/js/main.js
+++ assets/template/js/main.js
@@ -22,3 +22,30 @@ document.querySelectorAll('.btn-search-close').forEach((element) => {
     sectionSearch.classList.remove('open');
   });
 });
+//images
+let setImg = () => {
+  document.querySelectorAll('.img-data-src').forEach((box) => {
+    src = box.getAttribute('data-src');
+    box.setAttribute('src', src);
+  });
+};
+
+let setBg = () => {
+  document.querySelectorAll('.set-bg').forEach((box) => {
+    bg = box.getAttribute('data-setbg');
+    box.style.backgroundImage = "url('" + bg + "')";
+  });
+};
+
+setImg();
+setBg();
+//images
+document
+  .querySelectorAll(
+    ".content a[href$='.jpg'],.content a[href$='.jpeg'],.content a[href$='.png'], .content a[href$='.gif'], .content a[href$='.webP'], .content a[href$='.webp']"
+  )
+  .forEach((box) => {
+    box.setAttribute('data-fancybox', 'gallery');
+  });
+
+Fancybox.bind('[data-fancybox]');
